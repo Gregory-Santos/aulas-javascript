cd8ea5394f555ab9aaa04b519820f78cddebc4d5
Gregory <gregorysantos575@gmail.com>
2026-05-21 16:11:11 -0300

    Add files via upload

---
+++ aula-javascript/js/script.js
@@ -0,0 +1,36 @@
+// Variáveis e tipos de dados
+let nome = "Gregory";
+
+let idade = 16;
+
+let hobbie = "Tocar baixo e violão";
+
+console.log("Olá, meu nome é Gregory, tenho 16 anos e meus hobbies são: tocar baixo e violão.");
+
+// Operadores
+const PI = 3.14159; let raio = 5; let area = PI * raio * raio; console.log(area);
+
+// Tipos de dados
+console.log(typeof nome); // string
+
+console.log(typeof idade); // number
+
+console.log(typeof hobbie); // string
+
+// Conversão de tipos
+let stringNumber = "50";
+
+let number =
+
+Number(stringNumber);
+
+let resultado = number + 10;
+
+console.log(resultado); // 60
+
+let mensagem = "Fora";
+{ let mensagem = "Dentro";
+    console.log(mensagem); }
+console.log(mensagem);
+
+console.log(banANa, trezentOS, moToR);
